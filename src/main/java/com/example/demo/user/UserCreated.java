4ef42e1fd0c26d44c06f23022adf30753b0b836a
TungNS1 <tungns1@hblab.vn>
2026-06-18 13:38:25 +0700

    a

--- src/main/java/com/example/demo/user/UserCreated.java
+++ src/main/java/com/example/demo/user/UserCreated.java
@@ -2,7 +2,6 @@ package com.example.demo.user;
 
 import org.springframework.modulith.events.Externalized;
 
-@Externalized("users::#{#id}") 
+@Externalized("users::#{#this.user.id}") 
 public record UserCreated(User user) {
-    
 }
